a51ee1cb983cf92b8ddbfc887dff8b0b08527fa6
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-03-08 12:46:36 -0500

    challenge 3 strings

--- strings_3.js
+++ strings_3.js
@@ -1,3 +1,4 @@
+/*
 ## The challenge:  
    
   For this challenge, create a file named strings.js.  
@@ -8,6 +9,8 @@
    
   Use console.log to print the variable someString to the terminal.  
    
-  Check to see if your program is correct by running this command:  
-   
-  javascripting verify strings.js  
+*/
+
+var someString = 'this is a string';
+console.log(someString);
+
