52bfeef50534453588b41fec9598018e0433eb7e
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-03-08 12:45:18 -0500

    challenge 2 variables

---
+++ strings_3.js
@@ -0,0 +1,13 @@
+## The challenge:  
+   
+  For this challenge, create a file named strings.js.  
+   
+  In that file create a variable named someString like this:  
+   
+     var someString = 'this is a string';  
+   
+  Use console.log to print the variable someString to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+  javascripting verify strings.js  
